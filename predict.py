4838acceafb3d293b7a5e051b54976e450409aa4
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-01-08 14:56:57 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,19 +1,22 @@
-from keras.layers import Input
-from frcnn import FRCNN 
+#-------------------------------------#
+#       对单张图片进行预测
+#-------------------------------------#
+from frcnn import FRCNN
 from PIL import Image
 
 frcnn = FRCNN()
-
 
 while True:
     img = input('Input image filename:')
     try:
         image = Image.open(img)
+        #-------------------------------------#
+        #   转换成RGB图片，可以用于灰度图预测。
+        #-------------------------------------#
+        image = image.convert("RGB")
     except:
         print('Open Error! Try again!')
         continue
     else:
         r_image = frcnn.detect_image(image)
         r_image.show()
-frcnn.close_session()
-    
