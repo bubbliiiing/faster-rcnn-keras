2875f2f230c598e7964fd4479439171f12244f5c
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-11-17 00:33:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -167,6 +167,12 @@ if __name__ == "__main__":
         start_epoch = Init_Epoch
         end_epoch   = Freeze_Epoch
 
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
+
         model_rpn.compile(
             loss = {
                 'classification': rpn_cls_loss(),
@@ -184,12 +190,6 @@ if __name__ == "__main__":
 
         gen     = FRCNNDatasets(train_lines, input_shape, anchors, batch_size, num_classes, train = True).generate()
         gen_val = FRCNNDatasets(val_lines, input_shape, anchors, batch_size, num_classes, train = False).generate()
-
-        epoch_step      = num_train // batch_size
-        epoch_step_val  = num_val // batch_size
-
-        if epoch_step == 0 or epoch_step_val == 0:
-            raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         for epoch in range(start_epoch, end_epoch):
@@ -210,6 +210,12 @@ if __name__ == "__main__":
         start_epoch = Freeze_Epoch
         end_epoch   = UnFreeze_Epoch
 
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
+
         model_rpn.compile(
             loss = {
                 'classification': rpn_cls_loss(),
@@ -227,12 +233,6 @@ if __name__ == "__main__":
 
         gen     = FRCNNDatasets(train_lines, input_shape, anchors, batch_size, num_classes, train = True).generate()
         gen_val = FRCNNDatasets(val_lines, input_shape, anchors, batch_size, num_classes, train = False).generate()
-
-        epoch_step      = num_train // batch_size
-        epoch_step_val  = num_val // batch_size
-
-        if epoch_step == 0 or epoch_step_val == 0:
-            raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         for epoch in range(start_epoch, end_epoch):
